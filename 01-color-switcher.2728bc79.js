f0aff8d9d1733387beb2ef0e1ad087a3fd32fbaf
Sergvah <104735864+Sergvah@users.noreply.github.com>
2022-09-29 15:45:37 +0000

    Deploying to gh-pages from @ Sergvah/goit-js-hw-09@97c38652f7b4e6aab68fae120fbb9b4836de8776 🚀

---
+++ 01-color-switcher.2728bc79.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")},n=null;t.btnStart.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.btnStop.addEventListener("click",(function(){clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.2728bc79.js.map
