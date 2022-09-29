97c38652f7b4e6aab68fae120fbb9b4836de8776
Sergvah <104735864+Sergvah@users.noreply.github.com>
2022-09-29 18:45:06 +0300

    task-1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+'use strict';
 
+const button = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+
+let btnChangeColors = null;
+
+function startChangeColor() {
+  btnChangeColors = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function stopChangeColor() {
+  clearInterval(btnChangeColors);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+button.btnStart.addEventListener('click', startChangeColor);
+button.btnStop.addEventListener('click', stopChangeColor);
