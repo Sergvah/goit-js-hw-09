da65a5b21a17ca4854bd4580a8d808e1559494fb
Sergvah <104735864+Sergvah@users.noreply.github.com>
2022-10-03 15:12:05 +0300

    task-3 well done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,61 @@
+'use strict';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formSubmit = document.querySelector('.form');
+const formDelay = document.querySelector('[name="delay"]');
+const formStep = document.querySelector('[name="step"]');
+const formAmount = document.querySelector('[name="amount"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+// for (let i = 0; i <= 20; i += 5) {
+//   console.log(i);
+// }
+
+function onSubmit(event) {
+  event.preventDefault();
+  // if (
+  //   formDelay.value === '' ||
+  //   formStep.value === '' ||
+  //   formAmount.value === ''
+  // ) {
+  //   Notify.failure('Введите данные');
+  //   return;
+  // }
+  if (
+    Number(formDelay.value) < 0 ||
+    Number(formStep.value) < 0 ||
+    Number(formAmount.value) <= 0
+  ) {
+    Notify.failure('Введите положительные числа');
+    return;
+  }
+
+  let delay = Number(formDelay.value);
+  for (let counter = 1; counter <= Number(formAmount.value); counter++) {
+    console.log(delay);
+    createPromise(counter, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(formStep.value);
   }
 }
+formSubmit.addEventListener('submit', onSubmit);
